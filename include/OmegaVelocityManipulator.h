b842472ace9872a40b9cb14bed6bcdd819f3ebef
Micah Corah <micahcorah@gmail.com>
2014-03-29 14:54:42 -0400

    adding OmegaVelocity manipulator

---
+++ include/OmegaVelocityManipulator.h
@@ -0,0 +1,11 @@
+#ifndef OMEGAVELOCITYMANIPULATOR_H
+#define OMEGAVELOCITYMANIPULATOR_H 
+
+#include "BeepHive.h"
+#include "Manipulator.h"
+
+class OmegaVelocityManipulator : public Manipulator {
+  void update();
+};
+
+#endif /* OMEGAVELOCITYMANIPULATOR_H */
---
+++ src/OmegaVelocityManipulator.cpp
@@ -0,0 +1,42 @@
+#include <math.h>
+
+#include "BeepHive.h"
+#include "OmegaVelocityManipulator.h"
+
+/*!
+ * This method updates the state based on a radial and linear velocity
+ *
+ * The inputs to the state vector are:
+ * radial velocity, linear velocity
+ * omega is in the counterclockwise direction
+ * velocity is in the local X direction
+ */
+void OmegaVelocityManipulator::update()
+{
+  double delta_t = model->timeSinceLastUpdate();
+  double omega = inputs[0];
+  double velocity = inputs[1];
+  double theta = beep->getState("theta");
+  double x = beep->getState("x");
+  double y = beep->getState("y");
+
+
+  //Calculate change in position in the local frame
+  double delta_theta = theta * delta_t;
+  double radius = velocity / omega;
+  // alpha is the angle of the vector to the new position in the local reference
+  // frame
+  double alpha = delta_theta / 2;
+  // r is the lenght of the vector to the new position
+  double r = 2 * radius * sin(delta_theta / 2);
+
+  double local_x = r * cos(alpha);
+  double local_y = r * sin(alpha);
+
+  //Transform to global frame
+  double delta_x = (cos(theta) * local_x - sin(theta) * local_y);
+  double delta_y = (sin(theta) * local_x + cos(theta) * local_y);
+  beep->setState("x",x+delta_x);
+  beep->setState("y",y+delta_y);
+  beep->setState("theta",theta+delta_theta);
+}
