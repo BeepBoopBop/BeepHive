5443d7097d4e2ae2bb607d116468dbb093e495f3
Micah Corah <micahcorah@gmail.com>
2014-03-30 21:24:11 -0400

    Makig BeepLayer and SyncLayer friends of World with private constructors so that users will not be able to instantiate them erroneously

--- include/BeepLayer.h
+++ include/BeepLayer.h
@@ -4,15 +4,26 @@
 
 typedef char BeepTree;
 
+/*!
+ * The beep layer is responsible for initiating updates to all beeps
+ *
+ * This is a special layer that users are not able to instantiate as it relates
+ * to the beep data structure within the World class. Having multiple BeepLayers
+ * would likely result in buggy execution.
+ */
 class BeepLayer : public DiscreteTimeLayer {
+  friend class World;
   public:
-    BeepLayer();
-    BeepLayer(double event_frequency);
     virtual ~BeepLayer();
 
     virtual void update(const Event* event);
 
   private:
+    BeepLayer();
+    BeepLayer(double event_frequency);
+    BeepLayer(const BeepLayer& copy);
+    BeepLayer& operator=(const BeepLayer& copy);
+
     BeepTree beep_tree;
 };
 
--- include/SyncLayer.h
+++ include/SyncLayer.h
@@ -5,15 +5,25 @@
 #include "Event.h"
 
 /*!
- * The sync model implements syncronization with the GUI process
+ * The sync layer implements syncronization with the GUI process
+ *
+ * This is a special layer that users are not able to instantiate. The synclayer
+ * is automatically created by the world class if BeepHive was started with a
+ * GUI. There is no reason for the existence of multiple SyncLayers, so the user
+ * is explicitly prevented from instantiating more.
+ *
  */
 class SyncLayer : public DiscreteTimeLayer
 {
+  friend class World;
   public:
+    virtual void update(const Event* event);
+
+  private:
     SyncLayer();
     SyncLayer(double event_frequency);
-
-    virtual void update(const Event* event);
+    SyncLayer(const SyncLayer& copy);
+    SyncLayer& operator=(const SyncLayer& copy);
 };
 
 #endif /* SYNCMODEL_H */
