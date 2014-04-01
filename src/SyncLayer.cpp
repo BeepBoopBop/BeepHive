4abeb93979aa06963fe7058a4f2e2b05f35b9b47
Micah Corah <micahcorah@gmail.com>
2014-04-01 00:45:55 -0400

    Added basic messaging

--- src/SyncLayer.cpp
+++ src/SyncLayer.cpp
@@ -1,3 +1,7 @@
+#include <string>
+#include <boost/serialization/base_object.hpp>
+#include <boost/mpi.hpp>
+
 #include "BeepHive.h"
 #include "SyncLayer.h"
 
@@ -10,5 +14,18 @@ void SyncLayer::update(const Event* event)
   DEBUG("Updating SyncLayer");
   DiscreteTimeLayer::update(event);
   boost::mpi::communicator comm;
+
   comm.barrier();
+
+  //Send a message to the GUI
+  std::string send="SyncLayer just sent a message";
+  if(event->getTime() > 9){
+    send="exit";
+  }
+  gather(comm,send,&send,0);
+
+  //receive updates sent from GUI to all processes
+  std::string recv;
+  broadcast(comm,recv,0);
+  std::cout << "Received: " << recv << std::endl;
 }
